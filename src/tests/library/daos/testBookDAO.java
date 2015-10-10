41d364fb85951e866cd84ed9d9c41febe5db010c
EHummerston <magiktrev3@gmail.com>
2015-10-10 19:05:47 +1100

    Completed testing of Book and BookMapDAO
    Signed-off-by: EHummerston <magiktrev3@gmail.com>

---
+++ src/tests/library/daos/testBookDAO.java
@@ -0,0 +1,69 @@
+package tests.library.daos;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import library.daos.BookMapDAO;
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+public class testBookDAO {
+
+	BookMapDAO _bookDao;
+	IBookHelper _helper;
+	IBook _book;
+
+	String _author;
+	String _title;
+	String _callNumber;
+
+	@Before
+	public void setUp() throws Exception {
+		_helper = mock(IBookHelper.class);
+		_bookDao = new BookMapDAO(_helper);
+		_book = mock(IBook.class);
+
+		_author = "FName LName";
+		_title = "Title";
+		_callNumber = "call#";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void constructor_HelperNotNull_NoExceptions () {
+		BookMapDAO bookDao = new BookMapDAO (mock(IBookHelper.class));
+		assertTrue (bookDao instanceof BookMapDAO);
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void constructor_NullHelper_IllegalArgumentException () {
+		BookMapDAO bookDao = new BookMapDAO (null);
+		assertTrue (bookDao instanceof BookMapDAO);
+	}
+
+	@Test
+	public void addBook_NormalFlow_BookIdEqualsMapKey () {
+
+		when(_helper.makeBook(eq(_author), eq(_title), eq(_callNumber), anyInt())).thenReturn(_book);
+		ArgumentCaptor<Integer> argument = ArgumentCaptor.forClass(Integer.class);
+		
+		_bookDao.addBook(_author, _title, _callNumber);
+		verify(_helper).makeBook(any(String.class), any(String.class), any(String.class), argument.capture());
+		
+		assertEquals(_book, _bookDao.getBookByID(argument.getValue()));
+	}
+
+}
--- src/tests/library/daos/testLoanDAO.java
+++ src/tests/library/daos/testLoanDAO.java
@@ -21,12 +21,12 @@ public class testLoanDAO {
 	ILoanHelper _helper;
 	IBook _book;
 	IMember _borrower;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		_helper = mock(ILoanHelper.class);
 		_loanDao = new LoanMapDAO (_helper);
-		
+
 		_book = mock(IBook.class);
 		_borrower = mock(IMember.class);
 	}
@@ -40,49 +40,49 @@ public class testLoanDAO {
 		LoanMapDAO loanDao = new LoanMapDAO (mock(ILoanHelper.class));
 		assertTrue (loanDao instanceof LoanMapDAO);
 	}
-	
+
 	@Test (expected = IllegalArgumentException.class)
 	public void constructor_NullHelper_IllegalArgumentException () {
 		LoanMapDAO loanDao = new LoanMapDAO (null);
 		assertTrue (loanDao instanceof LoanMapDAO);
 	}
-	
+
 	@Test
 	public void createLoan_NormalFlow_HelperMakeLoanMethodCalled () {
 		ILoan loan = mock(ILoan.class);
 		when(_helper.makeLoan(eq(_book), eq(_borrower), any(Date.class), any(Date.class))).thenReturn(loan);
-		
+
 		ILoan loan2 = _loanDao.createLoan(_borrower, _book);		
 		verify(_helper).makeLoan(eq(_book), eq(_borrower), any(Date.class), any(Date.class));
 		assertEquals(loan, loan2);
 	}
-	
+
 	@Test (expected = IllegalArgumentException.class)
 	public void createLoan_NullBorrower_IllegalArgumentException () {
 		_loanDao.createLoan(null, _book);
 		assertTrue (_loanDao instanceof LoanMapDAO);
 	}
-	
+
 	@Test (expected = IllegalArgumentException.class)
 	public void createLoan_NullBook_IllegalArgumentException () {
 		_loanDao.createLoan(_borrower, null);
 		assertTrue (_loanDao instanceof LoanMapDAO);
 	}
-	
+
 	@Test
 	public void commitLoan_NormalFlow_LoanStoredWithUniqueId () {
 		ILoan loan = mock(ILoan.class);
-		 ArgumentCaptor<Integer> argument = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<Integer> argument = ArgumentCaptor.forClass(Integer.class);
 		_loanDao.commitLoan(loan);
 		verify(loan).commit(argument.capture());
 		assertEquals(_loanDao.getLoanByID(argument.getValue()),loan);
 	}
-	
+
 	@Test
 	public void getLoanById_NoneFound_ReturnsNull () {
 		assertEquals(_loanDao.getLoanByID(0),null);
 	}
-	
+
 	@Test
 	public void getLoanByBook_NoneFound_ReturnsNull () {
 		assertEquals(_loanDao.getLoanByBook(mock(IBook.class)),null);
